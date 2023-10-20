162df8043a1b6b0cb7a33e86e30ba1ff5dc0eafb
bartello88 <bartello88@gmail.com>
2023-10-20 14:13:10 +0200

    Add flask stuff

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 
 from main import sum
 
@@ -9,9 +10,9 @@ def test_sum2_function():
     assert sum(4,5) == 9
 
 
-
-
-
 def test_sum3_function():
     assert sum(4,10) == 14
 
+def test_flask_app():
+    r = requests.get("http://127.0.0.1:5000")
+    assert r.status_code==200
